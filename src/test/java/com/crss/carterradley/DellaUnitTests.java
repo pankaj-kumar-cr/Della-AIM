f65d25c505a50a010180d6c85355ef710bf8cb87
pankaj-kumar-cr <pankaj.kumar@carterradley.com>
2019-03-11 16:23:05 +0530

    Adding Dummy JUnit Tests for CI Demo

---
+++ src/test/java/com/crss/carterradley/DellaUnitTests.java
@@ -0,0 +1,35 @@
+package com.crss.carterradley;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DellaUnitTests {
+
+	@Test
+	public void test1() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test2() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test3() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test4() {
+		assertTrue(false);
+		
+	}
+	
+	@Test
+	public void test5() {
+		assertTrue(false);
+	}
+
+}
